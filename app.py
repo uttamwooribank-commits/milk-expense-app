39e8963ca8247ff0c5173f262b03372efcb48d8e
uttamwooribank-commits <uttam.wooribank@gmail.com>
2026-03-26 16:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if st.button("Generate Report"):
     file_name = f"Milk_Expense_{month_name}_{year}.xlsx"
 
     buffer = BytesIO()
-    df_final.to_excel(buffer, index=False, engine='openpyxl')
+    df_final.to_excel(buffer, index=False)
     buffer.seek(0)
 
     st.download_button(
